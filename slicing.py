149bf5eebabb903e6a301bf20eae55f2f5163559
jibrealC123 <cai5656@g.rit.edu>
2026-06-22 19:45:14 -0400

    slicing 1.2 version

--- slicing.py
+++ slicing.py
@@ -6,16 +6,11 @@
 msg = 'welcome to Python 101: Strings'
 
 # Print the built string using slicing
-print(msg[23:30].title(), msg[0:7].title(), msg[18:21], msg[11:17])
+print(msg[23:30].title(),msg[0:7].title(),msg[18:21],msg[11:17])
 
 # # Save the built string into a variable
 reseve_msg = msg[23:30].title() + " " + msg[0:7].title() + " " + msg[18:21] + " " + msg[11:17]
 
 # Print the result backwards
 print(reseve_msg[::-1])
-
-
-
-
-
-
+ 
