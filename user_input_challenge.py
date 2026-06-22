517937c827c97be9ac8855afc1d633c1200fe518
jibrealC123 <cai5656@g.rit.edu>
2026-06-22 14:38:14 -0400

    update 1.1 version

--- user_input_challenge.py
+++ user_input_challenge.py
@@ -4,17 +4,15 @@
 
 #  Distance coverter converting Km to Miles — Challenge Steps
 
-# - Take two inputs from user: Their first name and the distance in km
+# - Take two inputs from user: Their first name and the distance in km.
 name = input("What's your name? ")
 print(f"Hello, {name}! ")
-
-# - Create a distance converter converting Km to miles
 distance_km = float(input("Enter the km: "))
 
+# - Create a distance converter converting Km to miles in a round.
 distance_miles = distance_km / 1.609 
 distance_miles = round(distance_miles, 2)
 
-
-
+# - Print the outscreen to show the user that is converting to miles  so the user can understand.
 print("------------DISTANCE COVERTER-------------")
 print(f"{name.capitalize()}, {distance_km} km is equal to {distance_miles} miles! ")
